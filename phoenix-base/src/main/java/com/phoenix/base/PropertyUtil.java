67a16f94450df28432a4a78eed329f6a029496d0
pandora2j <casterrow@163.com>
2015-08-19 13:50:25 +0800

    新增模块

---
+++ phoenix-base/src/main/java/com/phoenix/base/PropertyUtil.java
@@ -0,0 +1,14 @@
+package com.phoenix.base;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+/**
+ * 
+ * @author Stephen Lau
+ * @date Aug 19, 2015
+ *
+ */
+public class PropertyUtil extends PropertyPlaceholderConfigurer {
+
+	
+}
