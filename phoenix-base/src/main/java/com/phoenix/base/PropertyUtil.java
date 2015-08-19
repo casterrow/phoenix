8e236c1db1902ed2e014c4899baef24ae199c518
pandora2j <casterrow@163.com>
2015-08-19 19:30:27 +0800

    配置修改

--- phoenix-base/src/main/java/com/phoenix/base/PropertyUtil.java
+++ phoenix-base/src/main/java/com/phoenix/base/PropertyUtil.java
@@ -1,14 +1,37 @@
 package com.phoenix.base;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 /**
  * 
- * @author Stephen Lau
+ * @author casterrow@163.com, Stephen Lau
  * @date Aug 19, 2015
+ * @version 1.0
  *
  */
 public class PropertyUtil extends PropertyPlaceholderConfigurer {
 
+	/**
+	 * Store key-value pairs from Spring container.
+	 */
+	private static Map<String, Object> ctxMap = new HashMap<String, Object>();
+	
+	
+	@Override
+	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props) throws BeansException {
+		
+		for (Object key : props.keySet()) {
+			//Put key-value pairs to a map.
+			ctxMap.put(key.toString(), props.getProperty(key.toString()));
+		}
+		
+	}
+	
 	
 }
--- phoenix-web/src/main/webapp/index.jsp
+++ phoenix-web/src/main/webapp/index.jsp
@@ -1,5 +1,23 @@
+
+<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+
+<!-- 引入jstl标签 -->
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!-- 获取根路径 -->
+<c:set var="ctx" value="${pageContext.request.contextPath}" />
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+"http://www.w3.org/TR/html4/loose.dtd">
+
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
+	<head>
+		<title>产品搜索页面</title>
+		<script type="text/javascript" charset="UTF-8" src="script/jquery-1.9.0.js"></script>
+		<script type="text/javascript" charset="UTF-8" src="script/search.js"></script>
+	</head>
+	<body>
+		<h2>搜索</h2>
+		
+	</body>
 </html>
