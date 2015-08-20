3547071d8f4ae36792499feaf32f6d6945b43b65
pandora2j <casterrow@163.com>
2015-08-21 00:57:51 +0800

    代码提交

--- phoenix-lucene/src/main/java/com/phoenix/lucene/indexer/Indexer.java
+++ phoenix-lucene/src/main/java/com/phoenix/lucene/indexer/Indexer.java
@@ -1,10 +1,18 @@
 package com.phoenix.lucene.indexer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -33,7 +41,7 @@ public class Indexer {
 	
 	/**
 	 * 创建索引
-	 * @author Stephen Lau
+	 * @author casterrow@163.com, Stephen Lau
 	 * @date Aug 20, 2015
 	 * @param indexDir
 	 * @param dataDir
@@ -63,21 +71,74 @@ public class Indexer {
 		writer.deleteAll();
 		indexDirectory(writer, dataDir);
 		int numIndexed = writer.numDocs();
-		writer.close();
+		IOUtils.closeQuietly(writer);
 		return numIndexed;
 	}
 	
 	
 	/**
 	 * 递归查找符合条件的文件
-	 * @author Stephen Lau
+	 * @author casterrow@163.com, Stephen Lau
 	 * @date Aug 20, 2015
 	 * @param writer
 	 * @param dir
 	 * @throws IOException
 	 */
 	private void indexDirectory(IndexWriter writer, File dir) throws IOException {
-		
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				indexDirectory(writer, file);
+			}
+			if (file.getName().endsWith(endStr)) {
+				indexFile(writer, file);
+			}
+		}
 	}
 
+	/**
+	 * 对文件创建索引
+	 * @author casterrow@163.com, Stephen Lau
+	 * @date Aug 20, 2015
+	 * @version 1.0
+	 * @param writer
+	 * @param file
+	 * @throws IOException 
+	 */
+	private void indexFile(IndexWriter writer, File file) throws IOException {
+		
+		//过滤隐藏文件、不存在文件或者不可读文件
+		if (file.isHidden()
+				|| !file.exists()
+				|| !file.canRead()) {
+			return;
+		}
+		
+		Document doc = new Document();
+		doc.add(new StringField(fieldName, file.getCanonicalPath(), Store.YES));
+		writer.addDocument(doc);
+	}
+	
+	/**
+	 * 读取文件
+	 * @author casterrow@163.com, Stephen Lau
+	 * @date Aug 20, 2015
+	 * @version 1.0
+	 * @param file
+	 * @param charset
+	 * @return
+	 * @throws IOException 
+	 */
+	public String readFile(File file, Charset charset) throws IOException {
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader isr = new InputStreamReader(fis, charset);
+		BufferedReader reader = new BufferedReader(isr);
+		String line = null;
+		StringBuffer buff = new StringBuffer();
+		while ((line = reader.readLine()) != null) {
+			buff.append(line);
+		}
+		IOUtils.closeQuietly(reader);
+		return buff.toString();
+	}
 }
